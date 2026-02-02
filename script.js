077d2f2f84886343b2ac1c3d5d5baf1c6436a0da
Sharath45454 <sharathssharath80@gmail.com>
2026-02-02 14:19:49 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//  alert('hello')
+console.log(123);
+console.log('java script code');
+console.log('true')
+var gratting ="hello"
+console.log(gratting);
+console.log([1,2,3,4,5]);
+console.log({a: 1, b: 2,c: 3});
---
+++ variables.js
@@ -0,0 +1,65 @@
+// var name ="sharath"
+// console.log(name);
+// name ="guvarav"
+// console.log(name);
+
+const { captureOwnerStack } = require("react");
+
+// var greeting;
+// greeting ="hello"
+// console.log(greeting);
+
+// var $name="johan"
+// console.log($name);
+
+// var firstName ="sharath"
+// console.log(firstName);
+// var _name = "ram"
+// console.log(_name);
+
+let FirstName ="raju"
+console.log(FirstName);
+
+// var greeting;
+// greeting ="hello"
+// console.log(greeting);
+
+// const person= {
+//     name: "sharath"
+// };
+// person.name = "rakesh"
+// console.log(person);
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+console.log(numbers);
+
+// Data types in java script
+// *primitive data types
+// stored directly in the location the variables acess
+// strored in the stack
+
+
+// String
+// number 
+// null
+// undefined
+// Boolean
+// Symbol
+
+
+// Refrence data types 
+// acssesd by refrence 
+
+// stored in heap and ponits to location in memory
+
+
+
+// Arryays 
+// object litrals
+// functions
+
+const name  = "ram"
+console.log(typeof name);
+
+
