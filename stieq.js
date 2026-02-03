00169668a280ee6cf2906c2e22aeebdcdee9b5c5
Sharath45454 <sharathssharath80@gmail.com>
2026-02-03 10:29:42 +0530

    stieq.js

---
+++ stieq.js
@@ -0,0 +1,20 @@
+// let a =2, b= 2, c=3;
+// let d ={name:"ramu"};
+// let e ={name: "ramu"};
+// let f = e;
+
+// console.log(a !==b);
+// console.log(a !== c);
+// console.log(d !== e);
+// console.log(f !== e);
+
+
+let m = 2;
+let n ="2";
+let p = true;
+let q =null;
+let r =undefined;
+
+console.log(m !==n);
+console.log(m !== p);
+console.log(q !==r);
