6ab9780d5d8f7958d22d5a0a0d0c234b71525d30
Sharath45454 <sharathssharath80@gmail.com>
2026-02-03 09:19:40 +0530

    arthmetc.js

---
+++ arthmetc.js
@@ -0,0 +1,11 @@
+let a =10;
+let b= 5;
+console.log('The arthmetic output'+(a+b));
+
+console.log(`the out of substraction ${a - b}`);
+
+console.log(`the out put of multiplication ==> ${a * b}`);
+
+console.log(`the out put od divistion ${a/b}`);
+
+console.log(`the out put of madulas ${a % b}`);
