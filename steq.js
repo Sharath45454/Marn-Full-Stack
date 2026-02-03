03eead6ab41d974935a2fe3cd9890f72b6e8ee41
Sharath45454 <sharathssharath80@gmail.com>
2026-02-03 10:16:29 +0530

    steq.js

--- steq.js
+++ steq.js
@@ -1,8 +1,41 @@
-let x = 5;
-let y = '6';
-let y1 = 5;
-console.log(x ===6);
-console.log(y === '5');
+// let x = 5;
+// let y = '6';
+// let y1 = 5;
+// console.log(x ===6);
+// console.log(y === '5');
 
-console.log(x=== y);
-console.log(x=== y1);
+// console.log(x=== y);
+// console.log(x=== y1);
+
+
+let a = 2,b =2, c=3;
+let d = {name: "ram"};
+let e = {name: "ram"};
+let f = e;
+let f1 = f;
+
+ console.log(a === b);
+ console.log(a===c);
+ console.log(d==e);
+ console.log(f===e);
+ console.log(f1===f);
+
+
+
+// let x = 2;
+
+// let y = '2';
+
+// let z = true;
+
+// let m = null;
+
+// let n = undefined;
+
+// console.log(x==y);
+
+// console.log(x== z);
+
+// console.log(m===n);
+
+// console.log (n==m);
