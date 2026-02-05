3530a9d3a424c3c093b2b99f55776cb96acf037f
Sharath45454 <sharathssharath80@gmail.com>
2026-02-05 09:20:51 +0530

    pop.js

---
+++ js_tutorial/pop.js
@@ -0,0 +1,24 @@
+// let a = [1, 2, 3,];
+// console.log(a)
+
+// let b = a.pop();
+// console.log(a);
+
+
+// let a = [];
+// let b = a.pop();
+// console.log(b);
+// console.log(a);
+
+
+let a = [];
+
+a.push(10);
+a.push(20);
+a.push(30);
+
+console.log(a);
+
+console.log(a.pop());
+console.log(a.pop());
+console.log(a.pop());
