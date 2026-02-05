8187c01c966834944c7a1e66e3416c834009f07c
Sharath45454 <sharathssharath80@gmail.com>
2026-02-05 17:14:18 +0530

    if-else1.js

---
+++ js_tutorial/if-else1.js
@@ -0,0 +1,11 @@
+let amount =500000;
+
+if(amount>=60000){
+    console.log("20% discont appled");
+}
+else if(amount>=2000){
+    console.log("10% discount");
+}
+else{
+    console.log("No discount");
+}
