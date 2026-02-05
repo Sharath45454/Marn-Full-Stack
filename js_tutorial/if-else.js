c7570e19834a5ed67f618028e6d0f64ab6bb46b6
Sharath45454 <sharathssharath80@gmail.com>
2026-02-05 17:04:00 +0530

    if-else.js

---
+++ js_tutorial/if-else.js
@@ -0,0 +1,8 @@
+let email = "admin@gmail.com";
+let password = "12345";
+
+if(email === "admin@gmail.com" && password === "12"){
+    console.log("Login successful");
+}else{
+    console.log("Invalid login");
+}
